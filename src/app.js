83eac5588aad9f9d54db4261040cb8b3c1feeaa7
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-12 16:39:10 +0530

    Exploring Routing

--- src/app.js
+++ src/app.js
@@ -2,11 +2,27 @@ const express = require('express');
 
 const app = express();
 
+// app.use("/",(req, res) => {
+//     res.send("Hey There!")
+// })
+
+app.use("/user",(req,res)=>{
+    res.send("Iam EveryWhere")
+})
+
+//* This will only handle get call to user
+app.get("/user",(req,res)=>{
+    res.send({first_name:"Subhashis",last_name:"Bhanj Deo"});
+})
+
+app.post("/user",(req,res)=>{
+    res.send("DATA IS SAVED");
+})
+
+
+//* THis is match all the HTTP method API
 app.use("/test", (req, res) => {
     res.send("Hello hello!")
-})
-app.use("/",(req, res) => {
-    res.send("Hey There!")
 })
 
 
