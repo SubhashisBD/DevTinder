f3b3c177cad338fe678fd18b10ae130138a912a4
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-14 12:13:49 +0530

    Initialize database

--- src/app.js
+++ src/app.js
@@ -2,6 +2,28 @@ const express = require('express');
 
 const app = express();
 const connectDB = require("./config/database")
+const User = require("./models/user")
+
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "Virat",
+        lastName: "Bhanj Deo",
+        email: "subh123bhanj@gmail.com",
+        password: "Abc123",
+
+    };
+    // *Creating a new Instance of the User Model
+    const user = new User(userObj)
+    try {
+
+        await user.save();
+        res.send("Document Sent Successful");
+    }
+    catch (err) {
+        res.status(400).send("Error:" + err.message)
+    }
+})
+
 
 connectDB()
     .then(() => {
