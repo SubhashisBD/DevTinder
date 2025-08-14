c7fd0226e406f76e4717b570dd0b9ff5d5eb33d0
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-14 10:17:04 +0530

    connect to db

---
+++ src/config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://subhashisbhanjdeo:9YowP5ahnBYH3CYE@namastenodejs.gvqibh9.mongodb.net/");
+};
+module.exports = connectDB;
