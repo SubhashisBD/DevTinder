0d538dc98c4c80c4a9d49680d5ec817826ba11c1
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-14 12:13:23 +0530

    named devTInder

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://subhashisbhanjdeo:9YowP5ahnBYH3CYE@namastenodejs.gvqibh9.mongodb.net/");
+    await mongoose.connect("mongodb+srv://subhashisbhanjdeo:9YowP5ahnBYH3CYE@namastenodejs.gvqibh9.mongodb.net/devTinder");
 };
 module.exports = connectDB;
