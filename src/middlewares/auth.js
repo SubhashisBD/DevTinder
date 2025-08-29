1a355bfe5fe7372dbb602dc1ea3bbe0ff2e1fe97
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-29 16:13:49 +0530

    User Auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,32 @@
-const adminAuth = (req, res, next) => {
-    console.log("Auth Identification")
-    let token = "xyz";
-    if (token === "xyz") {
-        res.status(404).send("Unauthorized request")
+const jwt = require("jsonwebtoken")
+const User = require("../models/user")
+
+const userAuth = async (req, res, next) => {
+
+    try {
+        // Read the tokens from the cookies
+        const { token } = req.cookies
+        if (!token) {
+            throw new Error("Token is not valid")
+        }
+
+        // Validate the tokens
+        const decodedObj = await jwt.verify(token, "SUBH@Tinder123")
+
+        // Finding the User By Id
+        const { _id } = decodedObj;
+        const user = await User.findById(_id)
+        
+        if (!user) {
+            throw new Error("User is not Present")
+        }
+        req.user = user;
+        next()
     }
-    else {
-        next();
+    catch (err) {
+        res.status(400).send("Error: " + err.message)
     }
+
 };
 
-const userAuth = (req, res, next) => {
-    console.log("User Auth Identification")
-    let token = "xyz";
-    if (token === "xyz") {
-        res.status(404).send("authorized request")
-    }
-    else {
-        console.log("Else Part")
-    }
-};
-
-module.exports = { adminAuth, userAuth };
+module.exports = { userAuth };
