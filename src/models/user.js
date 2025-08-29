f27d5673babbd5e72840381afa72a682c66bfe75
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-08-29 17:44:16 +0530

    added UserSchema Methods

--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 const userSchema = mongoose.Schema({
     firstName: {
         type: String,
         required: true,
-        minLenght:3,
-        maxLength:15
+        minLenght: 3,
+        maxLength: 15
     },
     lastName: {
         type: String,
-        minLenght:3,
-        maxLength:15
+        minLenght: 3,
+        maxLength: 15
     },
     emailId: {
         type: String,
@@ -19,8 +21,8 @@ const userSchema = mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
-        validate(value){
-            if(!validator.isEmail(value)){
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error("Invalid email address")
             }
         }
@@ -41,17 +43,30 @@ const userSchema = mongoose.Schema({
         type: String,
         default: "This is a default about Section"
     },
-    skills:{
-        type:[String]
+    skills: {
+        type: [String]
     },
-    age:{
-        type:Number
+    age: {
+        type: Number
     }
 },
     {
         timestamps: true
     },
 );
+
+userSchema.methods.getJWT = async function () {
+    const user = this;
+
+    const token = await jwt.sign({ _id: user._id }, "SUBH@Tinder123", { expiresIn: "1d" });
+
+    return token;
+};
+
+userSchema.methods.passwordHasher = async function (passwordByUser) {
+    const isPasswordValid = await bcrypt.compare(passwordByUser, this.password);
+    return isPasswordValid
+}
 
 const User = mongoose.model("User", userSchema);
 
