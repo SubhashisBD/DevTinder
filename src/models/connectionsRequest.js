5734293e43cb9507829229b21e84820748146537
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-09-01 17:31:16 +0530

    Requestion connection request

---
+++ src/models/connectionsRequest.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const connectionsRequest = new mongoose.Schema(
+    {
+        formUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+
+        },
+        toUserId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+
+        },
+        status: {
+            type: String,
+            required: true,
+            enum: {
+                values: ["ignored", "interested", "accepted", "rejected"],
+                message: `{VALUE} is incorrect in status type`,
+            },
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+// Why not to create Index for every field
+// * Making Index to make query Faster and why need?
+
+connectionsRequest.index({ formUserId: 1, toUserId: 1 })
+
+connectionsRequest.pre("save", function (next) {
+    const connectionsRequest = this
+    if (connectionsRequest.formUserId.equals(connectionsRequest.formUserId)) {
+        throw new Error("Cannot Send Connection Request to itself!")
+    }
+    next();
+})
+
+const ConnectionsRequestModel = new mongoose.model("ConnectionRequest", connectionsRequest);
+module.exports = ConnectionsRequestModel;
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,4 +1,49 @@
-const express = require("express")
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const connectionsRequest = require("../models/connectionsRequest");
+const User = require("../models/user");
 const requestRouter = express.Router();
+
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+    try {
+        const formUserId = req.user._id
+        const toUserId = req.params.toUserId
+        const status = req.params.status
+
+        const isAllowed = ["ignore", "interested"]
+        if (!isAllowed.includes(status)) {
+            return res.status(404).json({ message: "Invalid status type " + status })
+        }
+
+        const toUser = await User.findById(toUserId)
+        if (!toUser) {
+            res.status(404).json({ message: "User not found!" })
+        }
+
+        const existingConnectionRequest = await connectionsRequest.findOne({
+            $or: [{ formUserId, toUserId },
+            { formUserId: toUserId, toUserId: formUserId }
+            ]
+        })
+
+        if (existingConnectionRequest) {
+            return res.status(404).send({ message: " Connection Request Already Request" })
+        }
+
+        const connections = new connectionsRequest({
+            formUserId,
+            toUserId,
+            status
+        })
+        const data = await connections.save()
+        res.json({
+            message: "Connection sent Successfully",
+            data,
+        });
+    }
+    catch (err) {
+        res.status(404).send("ERROR: " + err.message)
+    }
+})
 
 module.exports = requestRouter; 
