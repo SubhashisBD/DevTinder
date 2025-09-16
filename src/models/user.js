316c35f81bb27bcffe4775201d9e598c95e82790
SubhashisBD <subhashisbhanjdeo@gmail.com>
2025-09-16 15:33:15 +0530

    expiresIn time update

--- src/models/user.js
+++ src/models/user.js
@@ -58,7 +58,7 @@ const userSchema = mongoose.Schema({
 userSchema.methods.getJWT = async function () {
     const user = this;
 
-    const token = await jwt.sign({ _id: user._id }, "SUBH@Tinder123", { expiresIn: "1d" });
+    const token = await jwt.sign({ _id: user._id }, "SUBH@Tinder123", { expiresIn: "4d" });
 
     return token;
 };
